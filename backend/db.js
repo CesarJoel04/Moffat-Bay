e6cd7b4a2183352aee8f78671830917eded564d0
CesarJoel04 <cesarestevez04@gmail.com>
2024-04-19 03:37:15 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: '127.0.0.1', // Use the IP address as mentioned in your MySQL Workbench details
   user: 'root', // Username remains the same
-  password: '#1Company', // Assuming the password provided is correct
+  password: 'Joel@2024', // Assuming the password provided is correct
   database: 'moffat_bay_lodge_db', // Ensure this matches the exact name of your database
   port: 3306 // Explicitly specifying the port, though this is the default
 });

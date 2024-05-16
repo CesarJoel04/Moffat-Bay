a22f9ee8a521a97dd31ab78efe4ceb97f2453bc7
Cesar Estevez <cesarestevez04@gmail.com>
2024-05-16 01:12:56 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,11 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: '127.0.0.1', // Use the IP address as mentioned in your MySQL Workbench details
   user: 'root', // Username remains the same
+<<<<<<< Updated upstream
   password: 'root1', // Assuming the password provided is correct
+=======
+  password: '', // Assuming the password provided is correct
+>>>>>>> Stashed changes
   database: 'moffat_bay_lodge_db', // Ensure this matches the exact name of your database
   port: 3306, // Explicitly specifying the port, though this is the default
 })
